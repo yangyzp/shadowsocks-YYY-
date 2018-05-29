0b68e0ef19250331d938fcb47235d0257deac247
yangyzp <513993979@qq.com>
2018-05-29 15:55:14 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -8,8 +8,8 @@ CLOUDSAFE = 1
 ANTISSATTACK = 0
 AUTOEXEC = 0
 
-MU_SUFFIX = 'v1m3cc2x'
-MU_REGEX = '%5m'
+MU_SUFFIX = 'download.windowsupdate.com'
+MU_REGEX = '%suffix'
 
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
 API_INTERFACE = 'glzjinmod'  # glzjinmod, modwebapi
